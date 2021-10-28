944e126b5d3dd75b873df98c190dd9247fec4b55
Isabella Mester <92123946+Isabella002@users.noreply.github.com>
2021-10-27 18:46:43 -0700

    Update ConversionTest.java

--- src/testing/ConversionTest.java
+++ src/testing/ConversionTest.java
@@ -1,4 +1,34 @@
 package testing;
+import java.util.Scanner;
 
-public class ConversionTest {
+public class ConversionTest { 
+    public static void main(String[] args) {
+        calculate_BMI();
+    }
+
+    private static void calculate_BMI() {
+        System.out.print("Please enter your weight in kg: ");
+        Scanner s = new Scanner(System.in);
+        float weight = s.nextFloat();
+        System.out.print("Please enter your height in cm: ");
+        float height = s.nextFloat();
+
+
+        float bmi = (100*100*weight)/(height*height);
+
+        System.out.println("Your BMI is: "+bmi);
+        printBMI_category(bmi);
+
+    }
+    private static void printBMI_category(float bmi) {
+        if(bmi < 18.5) {
+            System.out.println("You are underweight");
+        }else if (bmi < 25) {
+            System.out.println("You are normal");
+        }else if (bmi < 30) {
+            System.out.println("You are overweight");
+        }else {
+            System.out.println("You are obese");
+        }
+    }
 }
